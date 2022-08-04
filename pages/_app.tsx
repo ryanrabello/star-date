d2c2dfae71583fbce5d9f92e83479188435396ad
ryanrabello <ryan.s.rabello@gmail.com>
2022-08-04 15:53:04 -0700

    commented out code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,18 +26,6 @@ function MyApp({ Component, pageProps }) {
             maxHeight: 0.70,
             hueRotate: true,
       }
-      // shake: {
-      //   velocity: 10,
-      //   amplitudeX: 0.4,
-      //   amplitudeY: 0.4,
-      // },
-      // slice: {
-      //   count: 4,
-      //   velocity: 10,
-      //   minHeight: 0.02,
-      //   maxHeight: 0.40,
-      //   hueRotate: true,
-      // }
     });
   }, []);
 
